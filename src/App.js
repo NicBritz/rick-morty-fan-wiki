b97e9d86d0c358a8fa23834f01b059f254a35c25
Nic Britz <nico.britz@outlook.com>
2021-06-28 16:25:00 +0100

    :tada: initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchCharacters() {
+      const result = await axios(`https://rickandmortyapi.com/api/character`);
+
+      setData(result.data.results);
+    }
+    fetchCharacters();
+  }, []);
+
+  console.log(data && data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Character Wiki</h1>
+      <div className="grid">
+        {data &&
+          data.map((char) => (
+            <div className="grid-item">
+              <div className="card">
+                <img
+                  className="image"
+                  src={char.image}
+                  alt={`${char.name} portrait`}
+                />
+                <div key={char.id}>{char.name}</div>
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
-
-export default App;
